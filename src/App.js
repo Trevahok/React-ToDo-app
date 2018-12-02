3f337f10b45a05f117ea828d05178e34ae394908
Trevahok <vighneshss@gmail.com>
2018-12-02 21:31:03 +0530

    refactored for the better future

--- src/App.js
+++ src/App.js
@@ -2,103 +2,98 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 class App extends Component {
-    fetchNrenderList() {
-        let count = 0;
+    constructor(){
+        super();
+        this.state={
+            progress : 0,
+        }
+        this.fetchTaskList = this.fetchTaskList.bind(this);
+    }
+    componentDidMount(){
+        this.fetchTaskList();
+    }
+    fetchTaskList() {
+        let count =0;
         axios.get('http://127.0.0.1:8000/tasks')
             .then(result => {
+                for ( var i of result.data){
+                    if (i.completed){
+                        count++;
+                    }
+                }
                 this.setState({
-                    tasks: result.data.map(
-                        (d) => {
-                            if (d.completed) count++;
-                            return <Task d={d} />
-                        }),
                     rawData: result.data,
-                    completedCount: count,
-                    totalCount: result.data.length,
+                    progress: count*100/result.data.length,
                 });
                 console.log(this.state);
             })
     }
-
-    constructor() {
-        super()
-        this.state = {
-            tasks: (
-                <div className="btn mx-auto btn-secondary"> Loading ... </div>
-            ),
-            count: 0,
-            completedCount: 0,
-            totalCount: 1,
-
-        }
-        this.fetchNrenderList = this.fetchNrenderList.bind(this);
-    }
-    componentDidMount() {
-        this.fetchNrenderList();
-    }
     render() {
         return (
             <div className="container">
-                <Progress  progress={(this.state.completedCount / this.state.totalCount) * 100} />
-                <TaskList tasks={this.state.tasks} refetch={this.fetchNrenderList} />
+            <Progress progress = {this.state.progress} />
+            <TaskList data = {this.state.rawData} refetch = {this.fetchTaskList} />
             </div>
+
         );
     }
 }
 
+
 class Progress extends Component {
     render() {
         return (
+            <div>
             <div className="progress mb-4">
                 <div className="progress-bar bg-success" role="progressbar" style={{ width: this.props.progress + '%' }} aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
             </div>
         );
     }
 }
 
 class TaskList extends Component {
+    componentWillReceiveProps({data}) {
+      this.setState({...this.state,data})
+    }
     render() {
+              if (this.props.data != null) 
+                var rend = this.props.data.map((d)=> <Task completed = {d.completed} d={d} refetch = {this.props.refetch} />) ;
         // const renderList=this.state.tasks.map((task)=> <li>{task.desc}</li>);
         return (
             <div className="list-group">
-                {this.props.tasks}
-                <button onClick={this.props.refetch}>refresh</button>
+            {rend}
             </div>
         )
     }
 }
-class Task extends Component {
-        renderDesc(){
-            
-         return (this.state.completed)? (<strike>{this.props.d.desc} </strike>): this.props.d.desc;
-        }
-    constructor(props){
-        super(props);
-        this.state = {
-            completed: false,
-
-        }
+class Task extends Component{
+   componentWillReceiveProps({d}) {
+     this.setState({...this.state,d})
+   } 
+    renderDesc(){
+     return (this.props.completed)? (<strike>{this.props.d.desc} </strike>): this.props.d.desc;
     }
     deleteTask(url){
-        axios.delete(url);
+        axios.delete(url).then(
+        this.props.refetch()
+        );
     }
-    componentDidMount(){
-        this.setState({
-            completed: this.props.d.completed,
-            
-        })
+    completeTask(url){
+        console.log(this.props.d, url);
+        axios.put(url, {...this.props.d,completed : !this.props.completed})
     }
-    render() {
+    render(){
         return (
-            <li className='list-group-item text-center' key='{this.props.d.title}'>
-                <button className='btn float-left btn-outline-success border-0'>
-                    <span aria-hidden='true' className="fa fa-check"></span>
-                </button>
-                {this.renderDesc()}
-                <button className=' btn float-right btn-outline-danger border-0' onClick={()=>this.deleteTask(this.props.d.url)}>
-                    <span aria-hidden="true" className='fa fa-times' ></span>
-                </button>
-            </li>
+        <li className='list-group-item text-center' key='{this.props.d.title}'>
+            <button className='btn float-left btn-outline-success border-0' onClick= {()=> this.completeTask(this.props.d.url)}>
+                <span aria-hidden='true' className="fa fa-check"></span>
+            </button>
+            {this.renderDesc()}
+            <button className=' btn float-right btn-outline-danger border-0' onClick={()=>this.deleteTask(this.props.d.url)}>
+                <span aria-hidden="true" className='fa fa-times' ></span>
+            </button>
+        </li>
         )
     }
 }
