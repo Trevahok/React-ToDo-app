f0e8c2fe2044b1c5bd4e22bdc3e3967cf8e9c219
Trevahok <vighneshss@gmail.com>
2019-05-20 11:19:00 +0530

    add build folder

---
+++ build/precache-manifest.1ff31dc8c1acbc4550eabf8326cbc57d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a987a9ad35af20fb9e3",
+    "url": "/static/js/main.1a987a9a.chunk.js"
+  },
+  {
+    "revision": "69e5b96b3024f4c13f4d",
+    "url": "/static/js/1.69e5b96b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1551cf7f722b5bfcb129f8985e7e94ca",
+    "url": "/index.html"
+  }
+];
